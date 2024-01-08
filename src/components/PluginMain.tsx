3bf5d131dd66c04bb8b0ca8fb1eb80d6105d9771
OoL33 <alanalee@tutanota.com>
2024-01-07 21:36:26 -0500

    AuthCard refactored to ContractForm

--- src/components/PluginMain.tsx
+++ src/components/PluginMain.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ContractForm from "./ContractForm";
 
 const PluginMain = () => {
     return(
         <div>
-            <h1>PluginMain</h1>
+            <ContractForm />
         </div>
     )
 }
